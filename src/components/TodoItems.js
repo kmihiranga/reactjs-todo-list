971bc24db09862f1fe5a3584300b47a4fae86e59
kalana <kalana.mihiranga@hotmail.com>
2020-07-26 10:10:34 +0530

    make todo component

---
+++ src/components/TodoItems.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItems extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
